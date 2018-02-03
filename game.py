9920f8686bd5664284aaab52544c16d605ccd607
Jeffrey <jeffrey.ma@student.allenisd.org>
2018-02-03 14:23:36 -0600

    Added Description

--- game.py
+++ game.py
@@ -1,1 +1,9 @@
 print ("Hello!")
+
+
+
+access = input ("Enter Access Code. \n")
+if access input = (891983):
+    print (" End of the Game Message")
+else:
+    print ("Wrong Code. The Exit Module is Still Locked. \n")
