730b502a516a2adf9560c347bcc99a549fbe01a9
Jeffrey <jeffrey.ma@student.allenisd.org>
2018-02-03 14:22:51 -0600

    Added Items

--- items.py
+++ items.py
@@ -9,5 +9,23 @@ class Pipe(Weapon):
         self.name = "Brass Pipe"
         self.description = "A jagged brass pipe from a broken piping system."
         self.damage = 50
+        self.effect =
 
-class Knife(Weap)
+class Knife(Weapon):
+    def __init(self)
+        self.name = "Knife"
+        self.description = "It's not an actual knife, instead its a jagged piece of metal you found in someones body."
+        self.damage = 50
+        self.effect = bleeding
+
+class DemogorganTeeth(Weapon):
+    def __init(self)
+        self.name = "Dem Teeth"
+        self.descirption = "Teeth from a Demogorgan,lul"
+        self.damage = 7
+
+class Gun(Weapon):
+    def __init(self)
+        self.name = "Snake Gun"
+        self.description = "Gun that shoots black worms that rawl into the enemies mouth. Proceeds to eat the inside of the enemy. Yum"
+        self.damage = 100
