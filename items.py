2b338d92cd50e63b828cb0f8cb30482f4cb325a2
Jeffrey <jeffrey.ma@student.allenisd.org>
2018-02-18 22:50:41 -0600

    deleted effects

--- items.py
+++ items.py
@@ -6,18 +6,16 @@ class Weapon:
         return self.name
 class Pipe(Weapon):
     def __init(self)
-<<<<<<< HEAD
         self.name = "Brass Pipe"
         self.description = "A jagged brass pipe from a broken piping system."
         self.damage = 50
-        self.effect =
 
 class Knife(Weapon):
     def __init(self)
         self.name = "Knife"
         self.description = "It's not an actual knife, instead its a jagged piece of metal you found in someones body."
         self.damage = 50
-        self.effect = bleeding
+
 
 class DemogorganTeeth(Weapon):
     def __init(self)
@@ -30,7 +28,3 @@ class Gun(Weapon):
         self.name = "Snake Gun"
         self.description = "Gun that shoots black worms that rawl into the enemies mouth. Proceeds to eat the inside of the enemy. Yum"
         self.damage = 100
-=======
-        self.name = "Travel Sac"
-        self.description = "A sac that will become your Inventory"
->>>>>>> e40af553f1bfc52089f7002659c126095a318f4a
