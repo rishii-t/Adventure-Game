eb756eef628324e24c7a45229cdf6fba3b5f7f66
Steven J. Foland <stevenfoland@tsogiants.org>
2018-03-03 16:21:21 -0600

    Dr. Foland made some fixes.

--- formattext.py
+++ formattext.py
@@ -1,4 +1,4 @@
-from terminalize import get_terminal_size					# Allows us to determine terminal window size on any OS.
+from terminalsize import get_terminal_size					# Allows us to determine terminal window size on any OS.
 															# Adapted for Python 3.x from https://gist.github.com/jtriley/1108174
 
 import textwrap	# Gives us tools for formatting text in a much prettier fashion.
@@ -7,26 +7,26 @@ import textwrap	# Gives us tools for formatting text in a much prettier fashion.
 wrap_width = 0								# We will use this so that we can wrap text.
 
 
-# These functions exist only to help make the text print nicer in the terminal.
+# These functions exist only to help make the text print nicer in the terminal.		
 def get_width():
 	dimensions = get_terminal_size()
-	global wrap_width
+	global wrap_width 
 	wrap_width = dimensions[0] - 3				# Get the width of the user's window so we can wrap text.
 	return dimensions
-
-
+    
+	
 def clear_screen():
 	terminal = get_width()
-
+   
 	for i in range(terminal[1]):
 		print("")									# There are fancier ways to clear a screen, but this aligns our text where we want it at the bottom of the window.
 
-
+		
 def print_wrap(text):
 	get_width()
 	text = " ".join(text.split())				# Removes leading tabs from multiline strings.
 	print(textwrap.fill(text, wrap_width, replace_whitespace=True))
-
+	
 def print_center(text):
 	get_width()
 	text = " ".join(text.split())				# Removes leading tabs from multiline strings.
@@ -35,8 +35,8 @@ def print_center(text):
 			text = " " + text
 		print(text)
 	else:
-		print(textwrap.fill(text, wrap_width, replace_whitespace=True))
-
+		print(textwrap.fill(text, wrap_width, replace_whitespace=True))	
+ 
 if __name__ == "__main__":
 	sizex, sizey = get_terminal_size()
 	print('width =' + str(sizex) + ' height =' +  str(sizey))
--- parse.py
+++ parse.py
@@ -1,8 +1,6 @@
 import string				# Used to strip user inputs of punctuation.
 
-single_commands = {'go': {'north': ['north', 'n'], 'south': ['south', 's'], 'east': ['east', 'e'], 'west': ['west', 'w']}, 'check': {'inventory': ['inventory', 'i']}}
-
-game_commands = ['help', 'exit', 'quit', 'look']
+game_commands = {'help': ['help'], 'exit': ['exit', 'quit'], 'debug': ['debug']}
 
 verbs = {'go': ['go', 'go to', 'walk', 'head', 'move'], \
 	'take': ['take', 'pick up', 'grab', 'get'], \
@@ -10,64 +8,77 @@ verbs = {'go': ['go', 'go to', 'walk', 'head', 'move'], \
 	'drop': ['drop', 'put down', 'throw away'], \
 	'open': ['open'], \
 	'close': ['close', 'shut', 'slam'], \
+	'push': ['push', 'shove'], \
+	'pull': ['pull', 'tug'], \
 	'equip': ['equip', 'put on', 'wear'], \
 	'unequip': ['unequip', 'take off', 'remove'], \
 	'use': ['use', 'apply'], \
-	'check': ['check', 'look', 'examine', 'inspect'], \
-	'attack': ['attack', 'fight', 'kill']}
-
+	'consume': ['consume', 'eat', 'drink'], \
+	'unlock': ['unlock'], \
+	'lock': ['lock'], \
+	'check': ['check', 'look at', 'look in', 'look', 'examine', 'inspect'], \
+	'attack': ['attack', 'fight', 'kill'], \
+	'talk': ['talk to', 'talk', 'speak to', 'speak'], \
+	'buy': ['buy', 'purchase']}
+	
+implied_verbs = {'go': {'north': ['north', 'n'], 'south': ['south', 's'], 'east': ['east', 'e'], 'west': ['west', 'w']}, 'check': {'inventory': ['inventory', 'i'], 'hp': {'hp', 'health'}}}
+	
 prepositions = ["with", "to", "on", "from", "at"]
 
 articles = ["a", "an", "the"]
 
 
-
+	
 def identify_verbs(text):
 	found_verb = False
-
-	if(len(text) > 0):
+			
+	if(len(text) > 0):	
 		for character in string.punctuation:
-			text = text.replace(character, " ")		# Strip away all punctuation.
+			text = text.replace(character, " ")		# Strip away all punctuation.	
 		for verb in verbs.keys():							# This loop replaces any synonym that appears at the beginning of the user's input with their generic verb equivalent.
 			for synonym in verbs[verb]:						# e.g. "look at door" becomes "check door".
 				if text.startswith(synonym):
-					text = text[len(synonym):]		# Strip the synonym from the beginning of the user's input.
+					text = text[len(synonym):]				# Strip the synonym from the beginning of the user's input.
 					text = verb + text						# Put the generic verb equivalent at the beginning of the string.
-					found_verb = True						# Let the code later on know that we found a matching verb.
-
+					found_verb = True						# Let the code later on know that we found a matching verb.	
+					
+		if(not found_verb):
+			for command in game_commands.keys():
+				for synonym in game_commands[command]:
+					if text.startswith(synonym):
+						text = text[len(synonym):]			# Strip the synonym from the beginning of the user's input.
+						text = command + text				# Put the command equivalent at the beginning of the string.
+						found_verb = True					# Act as though we found a verb, even though commands may not be verbs.
+					
 	return [found_verb, text]
-
-
-
+	
+	
+	
 def strip_articles(text):
-
+	
 	if(len(text) > 0):
 		text = text.split(" ")								# Split the text up into a list of words.
-
+		
 		for index in range(len(text)):
 			for article in articles:
 				if(text[index] == article):
-					text[index] = ""						# Empty out any articles (e.g. "a" or "the").
-		text = list(filter(None, text))						# Get rid of any empty strings in the input text.
-
+					text[index] = ""						# Empty out any articles (e.g. "a" or "the").	
+		text = list(filter(None, text))						# Get rid of any empty strings in the input text.	
+	
 	text = " ".join(text)											# This is essentially the oppostite of the split(" ") method - it puts the sentence back together.
 	return text
-
-
-
-def parse_command(text, found_verb = True):
+	
+	
+	
+def parse_command(text, found_verb):					
 	if(len(text) > 0):
 		text = text.split(" ")										# Split the text up into a list of words.
-
-		if(len(text) == 1):										# Stop here if we have a special in-game command such as "help" or "quit".
-			for command in game_commands:
-				if(text[0] == command):
-					return text
-
-
-			for verb in single_commands.keys():					# This set of loops replaces any listed single word command synonyms with their generic command equivalent and adds their implied verb.
-				for command in single_commands[verb].keys():	# e.g. ["i"] becomes ["check", "inventory"]
-					for synonym in single_commands[verb][command]:
+		
+		if(len(text) == 1):										# If command is only one word long, we might have an implied verb or single word command.
+				
+			for verb in implied_verbs.keys():					# This set of loops replaces any listed single word command synonyms with their generic command equivalent and adds their implied verb.
+				for command in implied_verbs[verb].keys():		# e.g. ["i"] becomes ["check", "inventory"]
+					for synonym in implied_verbs[verb][command]:
 						if text[0] == synonym:
 							text.insert(0, verb)
 							text[1] = command
@@ -76,22 +87,22 @@ def parse_command(text, found_verb = True):
 				return text
 			else:
 				return None											# Return empty-handed if no matching commands were found.
-
-
+				
+						
 		elif(len(text) == 2):
 			if not found_verb:
 				text[0] = None									# Get rid of the verb if we do not recognize it.
-			return text											# Return what's left of the user's input. If a verb was found, this will return input in the form [VERB, NOUN].
+			return text											# Return what's left of the user's input. If a verb was found, this will return input in the form [VERB, NOUN]. 
 																# If no verb was found, this will return [None, NOUN]
-
+		
 		elif(len(text) > 2):
 			if not found_verb:
 				text[0] = None
-
+				
 			for preposition in prepositions:
 				if (text[1] == preposition):
 					return [text[0], None]				# Commands with a preposition following a verb are invalid.
-
+					
 			index = 2
 			while(index < len(text)):
 				found_preposition = False
@@ -105,17 +116,31 @@ def parse_command(text, found_verb = True):
 					text[index - 1] += " " + text[index]
 					text.pop(index)
 			return text
-
-		else:													# Return empty handed if there is no user input.
+			
+		else:													# Return empty handed if there is no user input.					
 			return None
 
+def pad_command(input):
+	for i in range(3):
+		try:
+			input[i]
+			
+		except IndexError:		# If we have reached beyond the boundaries of our list.
+			input.append(None)	# Add another None object to the end of the list.
+			
+		except:	# If the input is not a list (i.e. if the input is None).
+			input = [None]		# Make input a list.
+	return input
+			
 def get_command():
 	text = input('>> ').lower()
-
+	
 	[found_verb, user_input] = identify_verbs(text)
-
+	
 	user_input = strip_articles(user_input)
-
+	
 	user_input = parse_command(user_input, found_verb)
-
+	
+	user_input = pad_command(user_input)
+	
 	return [text, user_input]
